b7d816cb3fb02aabe84f1f9db3401ec8fe6d0e5f
Nick-Hergatt <nickmachergatt@gmail.com>
2019-06-13 13:58:48 -0400

    inital configuration

---
+++ src/main/java/nwe/generatorsWithoutNumber/GeneratorsWithoutNumberApplication.java
@@ -0,0 +1,13 @@
+package nwe.generatorsWithoutNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorsWithoutNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorsWithoutNumberApplication.class, args);
+	}
+
+}
